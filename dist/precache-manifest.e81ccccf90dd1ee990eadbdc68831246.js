ce9a5e74ae48fe5cdc932416760146e50d6c41e9
Louis Holdsworth <lh16556@essex.ac.uk>
2020-08-22 17:55:07 +0100

    add dist properly this time..

---
+++ dist/precache-manifest.e81ccccf90dd1ee990eadbdc68831246.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ae003d3e5917c3e75b",
+    "url": "/css/chunk-vendors.a7e2fd9b.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bec3b126b187d6678ceab6c256a83e38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a2a5198eefb6008d4ff",
+    "url": "/js/about.dc60dc75.js"
+  },
+  {
+    "revision": "ce836d64df7ddedd6085",
+    "url": "/js/app.31105ece.js"
+  },
+  {
+    "revision": "95ae003d3e5917c3e75b",
+    "url": "/js/chunk-vendors.f1892943.js"
+  },
+  {
+    "revision": "51aac391444c4dca1d73b0376e0b19d6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
