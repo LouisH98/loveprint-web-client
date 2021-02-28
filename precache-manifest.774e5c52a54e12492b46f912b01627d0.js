42b9fb1d3bd6bf45bd2644833e122812c2c3b2f7
Louis Holdsworth <33750128+LouisH98@users.noreply.github.com>
2021-02-28 12:52:27 +0000

    deploy

---
+++ precache-manifest.774e5c52a54e12492b46f912b01627d0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0c1447411c63c11639",
+    "url": "/loveprint-web-client/css/about.54d22f01.css"
+  },
+  {
+    "revision": "8531416156a3a273794d",
+    "url": "/loveprint-web-client/css/app.b9ec8745.css"
+  },
+  {
+    "revision": "073ff44594586dfc19d0",
+    "url": "/loveprint-web-client/css/chunk-vendors.1e5a2908.css"
+  },
+  {
+    "revision": "1ad88a35fd056e48b78d655249e82e6d",
+    "url": "/loveprint-web-client/img/loveprint-logo-white.1ad88a35.png"
+  },
+  {
+    "revision": "fcdaa49a22ca2ab895c5ed6a6a5944be",
+    "url": "/loveprint-web-client/img/loveprint-logo.fcdaa49a.png"
+  },
+  {
+    "revision": "1f9af44c97fbbc3309cff1f88199cb79",
+    "url": "/loveprint-web-client/index.html"
+  },
+  {
+    "revision": "bd0c1447411c63c11639",
+    "url": "/loveprint-web-client/js/about.f988fae8.js"
+  },
+  {
+    "revision": "8531416156a3a273794d",
+    "url": "/loveprint-web-client/js/app.86c893ee.js"
+  },
+  {
+    "revision": "073ff44594586dfc19d0",
+    "url": "/loveprint-web-client/js/chunk-vendors.a27f3750.js"
+  },
+  {
+    "revision": "e5e273f5833843fec89a8b52edc880ae",
+    "url": "/loveprint-web-client/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/loveprint-web-client/robots.txt"
+  }
+]);
