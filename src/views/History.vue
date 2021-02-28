1e22a8578a9c9fffee22e13e4fbcf3d5e23aff93
Louis Holdsworth <33750128+LouisH98@users.noreply.github.com>
2021-02-28 12:38:51 +0000

    Styling for history items - reflects the styling sent

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,46 +5,63 @@
       <v-container style="height: 100%">
         <v-row align="center" justify="center">
           <v-col v-for="historyItem in history" v-bind:key="historyItem.id" sm="6" cols="12" md="4" lg="4">
-            <v-card elevation="5" class="mx-6 my-1 rounded-lg">
-              <v-menu offset-y>
-                <template v-slot:activator="{on, attrs}">
-                  <v-btn icon  style="position: absolute; right:10px; top: 10px; z-index:5" v-on="on" v-bind="attrs">
-                    <v-icon>mdi-dots-horizontal</v-icon>
-                  </v-btn>
-                </template>
+            <v-card elevation="5" class="mx-3 my-0 rounded-lg">
+              <v-card-title class="pt-0 pb-0">
+                <v-spacer/>
+                <v-menu offset-y>
+                  <template v-slot:activator="{on, attrs}">
+                    <v-btn icon v-on="on" v-bind="attrs">
+                      <v-icon>mdi-dots-horizontal</v-icon>
+                    </v-btn>
+                  </template>
 
-                <!-- History Item Menu -->
-                <v-list>
-                  <v-list-item link @click="printMessage(historyItem)">
+                  <!-- History Item Menu -->
+                  <v-list>
+                    <v-list-item link @click="printMessage(historyItem)">
                       <v-list-item-icon class="pe-0 me-2">
                         <v-icon color="primary">mdi-printer</v-icon>
                       </v-list-item-icon>
-                    <v-list-item-content>
-                      <v-list-item-title color="primary">
-                        PRINT
-                      </v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item link @click="removeHistoryItem(historyItem)">
-                    <v-list-item-icon class="pe-0 me-2">
-                      <v-icon color="primary">mdi-delete</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                      <v-list-item-title color="primary">
-                        Remove
-                      </v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
+                      <v-list-item-content>
+                        <v-list-item-title color="primary">
+                          PRINT
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item link @click="removeHistoryItem(historyItem)">
+                      <v-list-item-icon class="pe-0 me-2">
+                        <v-icon color="primary">mdi-delete</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title color="primary">
+                          Remove
+                        </v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </v-card-title>
+
               <v-card-text class="pb-0">
                 <v-img :src="historyItem.image" :class="{'invert' : $vuetify.theme.dark}"/>
-                <span class="print-message">{{ historyItem.message }}</span>
+                <v-row :class="{
+                  'justify-start': historyItem.formatting.justify === 0,
+                  'justify-center': historyItem.formatting.justify === 1,
+                  'justify-end': historyItem.formatting.justify === 2,
+                }">
+                  <span class="print-message"
+                        :class="{
+                  'large-text' : historyItem.formatting.size === 2,
+                  'medium-text': historyItem.formatting.size === 1,
+                  'small-text': historyItem.formatting.size === 0
+                }">{{ historyItem.message }}</span>
+                </v-row>
+
+
               </v-card-text>
 
               <v-card-actions>
                 <v-spacer/>
-                <span class="grey--text" style="font-size: 0.8em">{{
+                <span class="grey--text mt-3" style="font-size: 0.8em">{{
                     new Date(historyItem.time).toLocaleDateString()
                     + ' @ ' + new Date(historyItem.time).toLocaleTimeString()
                   }}</span>
@@ -81,7 +98,7 @@ export default {
     this.history = getHistory();
 
     this.$root.$on('new-message', (messageObj) => {
-      this.history.push(messageObj)
+      this.history.unshift(messageObj)
     })
   },
   data: function () {
@@ -112,10 +129,21 @@ export default {
 
 .print-message {
   font-family: 'DotGothic16', sans-serif;
-  font-size: 24px
 }
 
 .invert {
   filter: invert(1)
+}
+
+.large-text {
+  font-size: 30px;
+}
+
+.medium-text {
+  font-size: 20px;
+}
+
+.small-text {
+  font-size: 10px;
 }
 </style>
